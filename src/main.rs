22d8570fd76d29a1a4ef6bd5092b26e5b46bc316
Michał Strug <michal.strug@rdx.works>
2024-05-31 16:51:13 +0200

    Added loop

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,18 @@
 use calc::eval_str;
 use std::io;
 
-fn main() {
-    println!("Enter equation:");
-    let mut buf = String::new();
-    match io::stdin().read_line(&mut buf) {
-        Ok(_) => match eval_str(&buf) {
-            Ok(result) => println!("Result: {result}"),
-            Err(e) => println!("Error occured: {e}"),
-        },
-        Err(e) => println!("Error occured: {e}"),
+fn main() -> Result<(), io::Error> {
+    loop {
+        println!("Enter equation (or Enter to finish):");
+        let mut buf = String::new();
+        if io::stdin().read_line(&mut buf)? == 1 {
+            break;
+        } else {
+            match eval_str(&buf) {
+                Ok(result) => println!("Result: {result}"),
+                Err(e) => println!("Error occured: {e}"),
+            }
+        }
     }
+    Ok(())
 }
